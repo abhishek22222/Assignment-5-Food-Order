56447d1b50dd4e315fd6028491c8dd83e18a567c
sam1raj <sam1raj9841@gmail.com>
2022-04-03 12:37:18 +0530

    first commit

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,28 @@
+import { useContext , reduce} from 'react';
+
+import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  // const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  //   return curNumber + item.amount;
+  // }, 0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>
+        {/* {numberOfCartItems} */}
+        3
+        </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
